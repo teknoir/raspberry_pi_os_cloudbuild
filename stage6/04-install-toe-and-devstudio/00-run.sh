394a54b4d1416b35ec50724e0debdce7804a55a6
Anders Åslund <anders.aslund@teknoir.ai>
2020-06-24 12:36:49 +0200

    Renamed HMQ to MQTT

--- stage6/04-install-toe-and-devstudio/00-run.sh
+++ stage6/04-install-toe-and-devstudio/00-run.sh
@@ -9,6 +9,6 @@ install -m 644 files/rsa_private.pem "${ROOTFS_DIR}/toe_conf/rsa_private.pem"
 
 mkdir -p "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests"
 install -m 644 files/devstudio-service.yaml "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests/devstudio-service.yaml"
-install -m 644 files/hmq-broker-service.yaml "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests/hmq-broker-service.yaml"
+install -m 644 files/mqtt-broker-service.yaml "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests/mqtt-broker-service.yaml"
 install -m 644 files/service-account.yaml "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests/service-account.yaml"
 install -m 644 files/toe-deployment.yaml "${ROOTFS_DIR}/var/lib/rancher/k3s/server/manifests/toe-deployment.yaml"
